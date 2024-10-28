f7aaf3a2c7d24ccbbc26bf48542ba4e3146fd607
Nguyễn Hữu Phúc <164979347+PhucNguyenHuu0@users.noreply.github.com>
2024-10-28 16:56:00 +0700

    ngay28/10/20024

---
+++ NHPhuc-QuanLyBanHang/Controllers/DonhangsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NHPhuc_QuanLyBanHang.Models;
+
+namespace NHPhuc_QuanLyBanHang.Controllers
+{
+    public class DonhangsController : Controller
+    {
+        private QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+
+        // GET: Donhangs
+        public ActionResult Index()
+        {
+            return View(db.Donhang.ToList());
+        }
+
+        // GET: Donhangs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donhang donhang = db.Donhang.Find(id);
+            if (donhang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donhang);
+        }
+
+        // GET: Donhangs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Donhangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaDH,Ngaydat,Tongtien")] Donhang donhang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donhang.Add(donhang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(donhang);
+        }
+
+        // GET: Donhangs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donhang donhang = db.Donhang.Find(id);
+            if (donhang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donhang);
+        }
+
+        // POST: Donhangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaDH,Ngaydat,Tongtien")] Donhang donhang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donhang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(donhang);
+        }
+
+        // GET: Donhangs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donhang donhang = db.Donhang.Find(id);
+            if (donhang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donhang);
+        }
+
+        // POST: Donhangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Donhang donhang = db.Donhang.Find(id);
+            db.Donhang.Remove(donhang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
